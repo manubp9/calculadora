68c215a11d3d675a562b04e08be7b32677439c1a
Manuel <manubp9@gmail.com>
2016-01-21 12:38:38 +0100

    añadido metodo que imprime por pantalla la suma de dos numeros

--- Calculadora.java
+++ Calculadora.java
@@ -48,4 +48,40 @@ public class Calculadora
         }
         System.out.println(num1);
     }
+    /**
+     * Implementa un método llamado `sumValuesInterval` que reciba dos parámetros enteros `a` y `b` 
+     * y devuelva la suma de los valores comprendidos entre dichos parámetros (ambos incluidos) y haz un commit. 
+     * En caso de que `b` sea menor que `a`, el programa debe intercambiarlos. En caso de que sean el mismo número, el valor que ha de retornarse es el mismo número. 
+     * Se debe comprobar que ambos parámetros son positivos o 0; 
+     * en caso contrario se informa por pantalla del error y se devuelve -1.
+     */
+    public int sumValuesInterval(int num1, int num2 )
+    {
+        int cont = 0;
+        
+        if (num1 == num2)
+        {
+            return (num1);
+        }
+        else if (num1 > num2)
+        {
+            int num3 = num1;
+            num1 = num2;
+            num2 = num3;
+        }
+        if ( (num1 < 0) || (num2 < 0 ))
+        {
+            System.out.println("ERROR,uno o los dos valores son negativos");
+            
+            num1= -1;
+        }else
+        {while (num1+cont <=num2  )
+        {
+            num1= num1 +cont;
+            cont ++;
+        }}
+        return num1;
+        
+        
+    }
 }
